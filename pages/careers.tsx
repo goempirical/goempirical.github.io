ead38337f95eb727692bc6d4b54adb091e8e2347
Enrique Mejia <jmtov4@gmail.com>
2022-03-29 11:23:11 -0500

    Enrique Mejia | `TextIcon` Component - Careers Adjustment - Adjusted texts for the `TextIcon` component on the `careers` page. - Added `Chess`, `Community`, `Gears`, `Jigsaw`, `Plan` and `Support` icons.

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -1,8 +1,10 @@
+import { texts } from '@constants/pages/careers';
+import { TIcons } from '@components/Icons/types';
 import DefaultLayout from '@components/Layouts/Default';
 import Hero from '@components/Hero';
-import Text from '@components/Text';
-import { texts } from '@constants/pages/careers';
 import Section from '@components/Section';
+import Text from '@components/Text';
+import TextIcon from '@components/TextIcon';
 import TextSection from '@components/TextSection';
 
 function Home() {
@@ -22,6 +24,19 @@ function Home() {
             content={talentProfileItem.content}
           />
         ))}
+      </Section>
+      <Section title={texts.ourApproach.title}>
+        <div className="flex flex-wrap gap-x-16 gap-y-12 justify-center">
+          {texts.ourApproach.items.map((ourApproachItem) => (
+            <TextIcon
+              key={ourApproachItem.title}
+              className="flex-1 md:basis-1/4 basis-1/2 max-w-sm"
+              icon={ourApproachItem.icon as TIcons}
+              title={ourApproachItem.title}
+              content={ourApproachItem.content}
+            />
+          ))}
+        </div>
       </Section>
     </DefaultLayout>
   );
