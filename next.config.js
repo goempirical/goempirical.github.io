2536154bd6bb182439caa3e64c62db519ad53d37
Enrique Mejia <jmtov4@gmail.com>
2022-04-01 19:38:25 -0500

    Enrique Mejia | Static Export Fix - Adjusted `next.config.js` to use a temporary loader for static images.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // This is a mock loader required as a workaround for the lack of a static export for Next.js. https://github.com/vercel/next.js/issues/21079
+    loader: 'akamai',
+    path: '/',
+  },
 };
 
 module.exports = nextConfig;
