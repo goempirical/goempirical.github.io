5864686edb19000810700bbf730538a335b386bc
Sebastian Pereyro <seba@goempirical.com>
2022-04-17 00:01:37 -0700

    Static export publish

--- source/components/ImageCard/index.tsx
+++ source/components/ImageCard/index.tsx
@@ -13,10 +13,10 @@ function ImageCard({
   return (
     <div className={clsx('flex flex-col items-center gap-3', className)}>
       <div className="flex items-center justify-center text-primary border-primary rounded-full h-60 w-60">
-        <img src={image} alt={title} className='clip-circle'/>
+        <img src={image} alt={title} className="clip-circle" />
       </div>
       <div className="flex flex-col items-center gap-2">
-        <a href={link} target="_blank">
+        <a href={link} target="_blank" rel="noreferrer">
         <Text variation="h4" className="text-center">{title}</Text>
         </a>
         <Text variation="p" className="opacity-70 text-center max-w-u360">{content}</Text>
