441b6b9c21f55464b195cb4f6718938fa7ca6da4
Sebastian Pereyro <seba@goempirical.com>
2023-06-20 09:48:09 -0700

    Static export publish

--- source/pages/_document.tsx
+++ source/pages/_document.tsx
@@ -7,6 +7,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-DDC6WY53TZ"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DDC6WY53TZ');
+          </script>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;600;700;800&family=Red+Hat+Text:ital,wght@0,300;0,400;0,600;1,300&display=swap" rel="stylesheet" />
